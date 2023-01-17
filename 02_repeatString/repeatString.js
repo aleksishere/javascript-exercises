9d37617d79cd844ac139ce8796800ebcfe4c19c8
Aleks <24192100+aleksishere@users.noreply.github.com>
2023-01-17 21:59:39 +0100

    Tasks 1-7 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, sum) {
+    let finalString = "";
+    if(sum < 0) {
+        return "ERROR";
+    } else {
+        for(let i=0; i<sum; i++) {
+            finalString = finalString + string;
+        }
+    }
+    return finalString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(minValue, maxValue) {
+    let sum = 0;
+    if((minValue > 0 && maxValue > 0) && (typeof minValue === "number" && typeof maxValue === "number")) {
+        if( minValue < maxValue) {
+            for(let i = minValue; i<=maxValue; i++) {
+                sum = sum + i;
+            }
+        } else if(minValue > maxValue) {
+            for(let i = maxValue; i<=minValue; i++) {
+                sum = sum + i;
+            }
+        }
+    } else {
+        return 'ERROR';
+    }
+    return sum;
 };
 
 // Do not edit below this line
